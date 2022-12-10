35f6289e003ee99e3d2cc1ad20b9f927ab2ba926
narendra-gardaswar1 <narendragardaswar@gmail.com>
2022-12-10 18:02:38 +0530

    fix: added auto-login queue in bull dashboard

--- src/core/middlewares/bull-board.middleware.ts
+++ src/core/middlewares/bull-board.middleware.ts
@@ -5,13 +5,18 @@ import { BullAdapter } from '@bull-board/api/bullAdapter';
 import { ExpressAdapter } from '@bull-board/express';
 
 import { Queue } from 'bull';
+import { AUTO_LOGIN_QUEUE } from '../../auto-login/auto-login.queue';
+import { InjectQueue } from '@nestjs/bull';
 
 @Injectable()
 export class BullBoardMiddleware implements NestMiddleware {
   private queues: Queue[] = [];
   serverAdapter = new ExpressAdapter();
-  constructor() {
-    this.queues = [];
+  constructor(
+    @InjectQueue(AUTO_LOGIN_QUEUE)
+    private readonly autoLoginQueue: Queue,
+  ) {
+    this.queues = [this.autoLoginQueue];
     createBullBoard({
       queues: this.queues.map(
         (queue) => new BullAdapter(queue, { allowRetries: true }),
